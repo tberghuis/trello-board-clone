e11ba34671d847021c00e99bb121bbd6c1c9b763
Tom Berghuis <thomas.berghuis@gmail.com>
2019-10-20 10:22:29 +1100

    .

---
+++ README.md
@@ -0,0 +1,10 @@
+https://tberghuis.github.io/trello-board-clone
+
+gif here
+
+list of tech
+
+discription of purpose
+
+summary how it works
+
--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import "./css/reset.css";
 import "./css/global.css";
 import React from "react";
 import Board from "./components/Board";
-// import GlobalStyle from "./globalStyle";
-import { Reset } from "styled-reset";
-// import Tmp from "./tmp";
+import styled from "styled-components";
 
 function App() {
   return (
-    <>
+    <Container>
+      <Heading>Trello Board Clone</Heading>
       <Board></Board>
-    </>
+    </Container>
   );
 }
 
 export default App;
+
+const Heading = styled.h1`
+  color: white;
+  margin: 10px;
+  text-align: center;
+`;
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;

e7311df53a24812fba821864098dfcba079df6a8
Tom Berghuis <thomas.berghuis@gmail.com>
2019-10-20 10:48:38 +1100

    .

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import "./css/global.css";
 import React from "react";
 import Board from "./components/Board";
 import styled from "styled-components";
+import github from "./img/github.png";
 
 function App() {
   return (
     <Container>
-      <Heading>Trello Board Clone</Heading>
+      <Heading>
+        <a
+          target="_blank"
+          href="https://github.com/tberghuis/trello-board-clone"
+        >
+          <img src={github} alt="Github" />
+        </a>
+        <span>Trello Board Clone</span>
+      </Heading>
       <Board></Board>
     </Container>
   );
@@ -15,10 +24,24 @@ function App() {
 
 export default App;
 
-const Heading = styled.h1`
+const Heading = styled.div`
+  font-family: roboto;
   color: white;
-  margin: 10px;
+  margin: 8px;
   text-align: center;
+  font-weight: 600;
+  font-size: 40px;
+  position: relative;
+  & img {
+    position: absolute;
+    background-color: white;
+    left: 0;
+    top: 0;
+    border-radius: 3px;
+    height: 40px;
+    width: 40px;
+    padding: 3px;
+  }
 `;
 
 const Container = styled.div`
